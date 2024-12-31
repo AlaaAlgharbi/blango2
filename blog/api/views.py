18efaff77d3c55ff80dbb373b76aafc22ffead01
unknown <algarbealaa@gmail.com>
2024-12-31 13:38:08 +0300

    finish related

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,14 @@
 from rest_framework import generics
 from rest_framework.authentication import SessionAuthentication
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
-from blog.api.serializers import PostSerializer
+from django.contrib.auth.models import User
+from blog.api.serializers import PostSerializer, UserSerializer
 from blog.models import Post
 
+class UserDetail(generics.RetrieveAPIView):
+    lookup_field = "email"
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class PostList(generics.ListCreateAPIView):
     authentication_classes = [SessionAuthentication]
